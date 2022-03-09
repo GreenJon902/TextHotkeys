9348f37aa1673e4c12c6f1324d51b252588a912f
greenjon902 <greenjon902@gmail.com>
2022-03-09 20:18:31 +0000

    Initial commit

---
+++ src/main/java/com/greenjon902/texthotkeys/client/TextHotkeysClient.java
@@ -0,0 +1,13 @@
+package com.greenjon902.texthotkeys.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TextHotkeysClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
