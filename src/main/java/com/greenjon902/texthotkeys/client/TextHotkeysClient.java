d122fbc0ae9752597f3bb03ba14cc2075e9d1fa1
greenjon902 <greenjon902@gmail.com>
2022-03-13 14:15:36 +0000

    added keys and lang and removed mixins

--- src/main/java/com/greenjon902/texthotkeys/client/TextHotkeysClient.java
+++ src/main/java/com/greenjon902/texthotkeys/client/TextHotkeysClient.java
@@ -3,11 +3,37 @@ package com.greenjon902.texthotkeys.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.LiteralText;
+import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class TextHotkeysClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
+        KeyBinding reloadKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.texthotkeys.reload",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_R,
+                "category.textHotkeys"
+        ));
 
+        KeyBinding openConfigBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.texthotkeys.openConfig",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_C,
+                "category.textHotkeys"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openConfigBinding.wasPressed()) {
+                client.player.sendMessage(new LiteralText("Opening config file..."), false);
+                java.awt.Desktop.getDesktop().edit();
+            }
+        });
     }
 }
