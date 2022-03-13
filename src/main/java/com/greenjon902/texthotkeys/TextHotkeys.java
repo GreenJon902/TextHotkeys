768c6ba1574eec5ddad10b4ca50b9cf2c83f1e7e
greenjon902 <greenjon902@gmail.com>
2022-03-13 13:42:23 +0000

    removed window stuff

--- src/main/java/com/greenjon902/texthotkeys/TextHotkeys.java
+++ src/main/java/com/greenjon902/texthotkeys/TextHotkeys.java
@@ -1,22 +1,10 @@
 package com.greenjon902.texthotkeys;
 
-import com.greenjon902.texthotkeys.client.configScreen.ConfigGui;
-import com.greenjon902.texthotkeys.client.configScreen.ConfigScreen;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import org.lwjgl.glfw.GLFW;
 
 public class TextHotkeys implements ModInitializer {
     @Override
     public void onInitialize() {
-        KeyBinding configMenuKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.texthotkeys.open.config",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_R,
-                "category.examplemod.test"
-        ));
+
     }
 }
