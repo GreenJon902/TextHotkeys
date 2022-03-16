16011955c8d81203bca36e81d7b5653e7936366d
greenjon902 <greenjon902@gmail.com>
2022-03-16 15:50:28 +0000

    added open config file

--- src/main/java/com/greenjon902/texthotkeys/client/TextHotkeysClient.java
+++ src/main/java/com/greenjon902/texthotkeys/client/TextHotkeysClient.java
@@ -5,16 +5,27 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.LiteralText;
+import org.apache.commons.io.FileUtils;
 import org.lwjgl.glfw.GLFW;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 @Environment(EnvType.CLIENT)
 public class TextHotkeysClient implements ClientModInitializer {
 
+    static File configFile = FabricLoader.getInstance().getConfigDir().resolve("textHotkeys.ini").toFile();
+    static URL defaultConfigFile = TextHotkeysClient.class.getResource("/textHotkeys.ini");
+
     @Override
     public void onInitializeClient() {
+
         KeyBinding reloadKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.texthotkeys.reload",
                 InputUtil.Type.KEYSYM,
@@ -29,11 +40,45 @@ public class TextHotkeysClient implements ClientModInitializer {
                 "category.textHotkeys"
         ));
 
+        reload();
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openConfigBinding.wasPressed()) {
+                assert client.player != null;
+
                 client.player.sendMessage(new LiteralText("Opening config file..."), false);
-                java.awt.Desktop.getDesktop().edit();
+                check_file();
+                System.out.println(configFile.exists());
+
+                Runtime runtime = Runtime.getRuntime();
+                try {
+                    runtime.exec("notepad.exe " + configFile.getAbsolutePath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    client.player.sendMessage(new LiteralText("Failed to open config file!"), false);
+                }
+
             }
         });
+    }
+
+    public void reload() {
+        System.out.println(configFile);
+        check_file();
+    }
+
+    public void check_file() {
+        System.out.println(configFile.exists());
+        if (!configFile.exists()) {
+            configFile.getParentFile().mkdirs();
+            System.out.println(defaultConfigFile);
+            System.out.println(configFile);
+
+            try {
+                FileUtils.copyURLToFile(defaultConfigFile, configFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
