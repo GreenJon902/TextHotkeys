c9eee8a5bb2bdf8e3205018073f89ce91db9d4dd
greenjon902 <greenjon902@gmail.com>
2022-03-17 18:36:20 +0000

    optomized imputs

--- src/main/java/com/greenjon902/texthotkeys/client/TextHotkeysClient.java
+++ src/main/java/com/greenjon902/texthotkeys/client/TextHotkeysClient.java
@@ -5,9 +5,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.impl.client.keybinding.KeyBindingRegistryImpl;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.TranslatableText;
@@ -17,12 +15,7 @@ import org.lwjgl.glfw.GLFW;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.net.Inet4Address;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 @Environment(EnvType.CLIENT)
