6c05740abe0a49a339306fda87c40a2eacef8229
greenjon902 <greenjon902@gmail.com>
2022-03-17 15:51:33 +0000

    added start of config loading

--- src/main/java/com/greenjon902/texthotkeys/client/TextHotkeysClient.java
+++ src/main/java/com/greenjon902/texthotkeys/client/TextHotkeysClient.java
@@ -5,17 +5,19 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.impl.client.keybinding.KeyBindingRegistryImpl;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
 import org.apache.commons.io.FileUtils;
 import org.lwjgl.glfw.GLFW;
 
-import java.awt.*;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Scanner;
 
 @Environment(EnvType.CLIENT)
 public class TextHotkeysClient implements ClientModInitializer {
@@ -46,33 +48,54 @@ public class TextHotkeysClient implements ClientModInitializer {
             while (openConfigBinding.wasPressed()) {
                 assert client.player != null;
 
-                client.player.sendMessage(new LiteralText("Opening config file..."), false);
+                client.player.sendMessage(new TranslatableText("chat.config.open"), false);
                 check_file();
-                System.out.println(configFile.exists());
 
                 Runtime runtime = Runtime.getRuntime();
                 try {
                     runtime.exec("notepad.exe " + configFile.getAbsolutePath());
                 } catch (IOException e) {
                     e.printStackTrace();
-                    client.player.sendMessage(new LiteralText("Failed to open config file!"), false);
+                    client.player.sendMessage(new TranslatableText("chat.config.open.failed"), false);
                 }
+            }
 
+            while (reloadKeyBinding.wasPressed()) {
+                assert client.player != null;
+
+                client.player.sendMessage(new TranslatableText("chat.config.reload"), false);
+                check_file();
+                reload();
+
+
+                try {
+                    KeyBindingRegistryImpl.class.getDeclaredField("moddedKeyBindings").setAccessible(true);
+
+                } catch (NoSuchFieldException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
 
     public void reload() {
-        System.out.println(configFile);
+        System.out.println("Reloading config file!");
         check_file();
+        try {
+            Scanner scanner = new Scanner(configFile);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void check_file() {
-        System.out.println(configFile.exists());
         if (!configFile.exists()) {
             configFile.getParentFile().mkdirs();
-            System.out.println(defaultConfigFile);
-            System.out.println(configFile);
 
             try {
                 FileUtils.copyURLToFile(defaultConfigFile, configFile);
